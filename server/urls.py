86a54754ddf9b4e7d1bad11e93462dd1aa816090
animesh288 <animesh.satish@gmail.com>
2022-10-27 18:04:18 +0530

    created django-app questions

--- server/urls.py
+++ server/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls'))
+    path('api/questions/',include('questions.urls'))
 ]
