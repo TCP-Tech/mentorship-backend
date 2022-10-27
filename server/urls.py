b4a34523051258803dfcdd357c1bad5ccdffa02a
Viraj Chandra <virajchandra51@gmail.com>
2022-10-27 20:40:10 +0530

    Created django questions app

--- server/urls.py
+++ server/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls'))
+    path('api/users/', include('users.urls')),
+    path('api/questions/', include('questions.url'))
 ]
