a7c3c8980ee3a8c700c70222574d2987e96f4bad
Aniket Kumar <kraniket123654@gmail.com>
2022-10-29 10:44:26 +0000

    did not add comma to resolve conflict

--- server/settings.py
+++ server/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'team.apps.TeamConfig',
     'questions'
+    'team.apps.TeamConfig',
 ]
 
 MIDDLEWARE = [
