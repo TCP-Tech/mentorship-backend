35a62497f1b905dc81d694ec402e272365e5a3c6
animesh288 <animesh.satish@gmail.com>
2022-11-01 15:13:29 +0530

    added leaderboard app

--- server/urls.py
+++ server/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
-    path('api/questions/',include('questions.urls'))
+    path('api/questions/',include('questions.urls')),
+    path('api/leaderboard/',include('leaderboard.urls'))
 ]
