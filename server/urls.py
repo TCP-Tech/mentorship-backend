8a2ca3aa5b9adc66b8ba1fe6d03f64c03427eb4b
animesh288 <animesh.satish@gmail.com>
2022-11-01 15:04:30 +0530

    resolved errors in questions app

--- server/urls.py
+++ server/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls'))
+    path('api/users/', include('users.urls')),
     path('api/questions/',include('questions.urls'))
 ]
