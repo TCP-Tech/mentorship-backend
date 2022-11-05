6715ec0a3f53aa335e0362d8182735005074e06d
Kuntash <kuntashtashi11@gmail.com>
2022-11-06 01:38:15 +0530

    Created Mentee Model in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Mentee model
+
+
+class Mentee(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(max_length=200)
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    team_id = models.BigIntegerField()
+    branch = models.CharField(max_length=200)
+    semester = models.BigIntegerField()
+    phone_number = models.BigIntegerField()
+    codechefID = models.TextField()
+    codeforcesID = models.TextField()
+    leetcodeID = models.TextField()
+    gfgID = models.TextField()
+    hackerrankID = models.TextField()
+    linkedinID = models.TextField()
+    score = models.BigIntegerField()
+    solved_q = models.BigIntegerField()
+    alloted_mentor_id = models.BigIntegerField()
+    rank = models.BigIntegerField()
+
