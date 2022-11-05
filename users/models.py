9ef7d8043dd260362ad205232d7d6a6c7b36b51e
Viraj Chandra <virajchandra51@gmail.com>
2022-11-06 02:40:42 +0530

    Mentor Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Mentor model
+
+class Mentor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(max_length=200)
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    alloted_team_ids = models.BigIntegerField()
+    branch = models.CharField(max_length=200)
+    semester = models.BigIntegerField()
+    phone_number = models.BigIntegerField()
+    codechefID = models.TextField()
+    codeforcesID = models.TextField()
+    leetcodeID = models.TextField()
+    gfgID = models.TextField()
+    hackerrankID = models.TextField()
+    linkedinID = models.TextField()
+    score = models.BigIntegerField()
+    total_q = models.BigIntegerField()
+    
