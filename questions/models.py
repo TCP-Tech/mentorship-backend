606c44e1ad577181112bfbb2fdca37b1f52804d7
animesh288 <animesh.satish@gmail.com>
2022-11-08 11:02:17 +0530

    updated Question model

--- questions/models.py
+++ questions/models.py
@@ -3,18 +3,22 @@ from django.db import models
 class Topic(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
+
 class Question(models.Model):
-    id=models.IntegerField(primary_key=True)
-    topic=models.CharField(max_length=200)
+    LEVELS=[
+        ('easy','easy'),
+        ('medium','medium'),
+        ('hard','hard'),
+        ]
+    topic=models.ForeignKey(Topic,models.CASCADE)
     name=models.CharField(max_length=200)
-    codechefLink=models.CharField(max_length=200)
-    codeforcesLink=models.CharField(max_length=200)
-    gfgLink=models.CharField(max_length=200)
-    leetcodeLink=models.CharField(max_length=200)
-    hackerrankLink=models.CharField(max_length=200)
-    global_check=models.BooleanField(default=False)
-    level=models.CharField(max_length=20)
-    mentor_id=models.IntegerField()
+    codechefLink=models.URLField(max_length=200)
+    codeforcesLink=models.URLField(max_length=200)
+    gfgLink=models.URLField(max_length=200)
+    leetcodeLink=models.URLField(max_length=200)
+    hackerrankLink=models.URLField(max_length=200)
+    level=models.CharField(choices=LEVELS,max_length=6)
+    # mentor_id=models.IntegerField()
     alloted_at=models.DateTimeField()
 
 
