a0bf29b5832f8a6761ae79f385c9a05deb926077
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2022-11-11 15:08:02 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
+    'users.apps.UsersConfig',
     'questions',
     'leaderboard',
     'team',
